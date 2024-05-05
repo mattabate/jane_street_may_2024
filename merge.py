e02ceb380742bf749ed85883da78e7b8f0aa8c2a
Matthew Abate <mabate13@gmail.com>
2024-05-04 22:44:04 -0400

    four rows, only one solution

--- merge.py
+++ merge.py
@@ -2,7 +2,7 @@ import json
 import tqdm
 
 
-ROW1 = 5
+ROW1 = 10
 ROW2 = ROW1 + 1
 
 rows_1_file = f"row_{ROW1}.json"
@@ -15,6 +15,7 @@ with open(rows_2_file) as f:
 
 
 col_rol = {
+    4: [1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1],
     5: [1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0],
     6: [0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0],
     7: [1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0],
